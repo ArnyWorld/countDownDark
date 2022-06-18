9d21cdf8f7a53582489d0638a64a7c5bf0df31d8
ArnyWorld <kings31lendy@gmail.com>
2022-06-17 21:08:19 -0400

    CountDown Dark JS

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.down'),
+container = document.querySelector('.container');
+
+
+//Fecha a futuro
+const countdownDate = new Date('06 19, 2022 20:46:00').getTime();
+
+let interval = setInterval(function(){
+    //Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
+
+    //Escribimos resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if(distance < 0){
+        clearInterval(interval);
+        container.style.opacity=0;
+        $finalMessage.style.transform = 'translateY(0)';
+
+    }
+}, 1000);
+
+let saludo="Arnaldo";
+console.log(saludo.slice(-2));
